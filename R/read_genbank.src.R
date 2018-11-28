cc90d92fec0b3eef9e2348fbfd86214a0584c562
jjlinscientist <jjlinscientist@protonmail.com>
2018-11-27 18:50:10 -0800

    added read_genbank function

---
+++ R/read_genbank.src.R
@@ -0,0 +1,38 @@
+#' simple genbank file reader
+#'
+#' extract DNAStringSet data from genbank files
+#' @param files path or list of paths to genbank files
+#' @export
+#' @examples
+#' random_DNA(c("example1.gb", "example2.gb"))
+
+read_genbank <- function(files)
+{
+	sequences <- lapply(files,
+			    function(file)
+			    {
+				    lines <- readLines(file)
+				    origin <- grep("^ORIGIN", lines) + 1
+				    stop <- grep("^//", lines) - 1
+				    sequence <- lines[origin:stop]
+				    sequence <- gsub("[0-9]", "", sequence)
+				    sequence <- gsub(" ", "", sequence)
+				    sequence <- paste(sequence, collapse = "")
+				    if(grepl("t|T", sequence) && !grepl("u|U", sequence))
+				    {
+					    string <- DNAString(sequence)
+				    }
+				    else if(grepl("u|U", sequence) && !grepl("t|T", sequence))
+				    {
+					    string <- RNAString(sequence)
+				    }
+				    else
+				    {
+					    stop("Cannot mix T and U in the same sequence")
+				    }
+				    return(string)
+			    }
+			    )
+	sequences <- DNAStringSet(sequences)
+	return(sequences)
+}
