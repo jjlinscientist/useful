6105f3f0f9d54b392a8563333e0aff1ef7f9c1bd
jjlinscientist <jjlinscientist@protonmail.com>
2018-12-04 14:32:41 -0800

    updated some man page formatting for read_fasta()

--- R/read_fasta.src.R
+++ R/read_fasta.src.R
@@ -1,12 +1,14 @@
-#' simple fasta file batch reader
+#' fasta batch Biostrings
 #'
-#' extract DNAStringSet data from a batch of fasta files - requires package Biostrings
+#' extract DNAStringSet data from a batch of fasta files: requires package Biostrings
 #' @param files path or list of paths to fasta files
 #' @param is_RNA logical argument to use RNAStringSet - default is FALSE
 #' @param output_list logical argument to concatenate list of XStringSets - default is FALSE
 #' @export
 #' @examples
-#' read_fasta(files = c("path/to/example1.fa", "path/to/example2.fa"), is_RNA = FALSE, output_list = FALSE)
+#' read_fasta(files = c("path/to/example1.fa", "path/to/example2.fa"), 
+#' 	      is_RNA = FALSE,
+#'	      output_list = FALSE)
 
 read_fasta <- function(files,
 		       is_RNA = FALSE,
