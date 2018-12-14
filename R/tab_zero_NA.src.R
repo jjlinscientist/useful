e7ada8d78f81951632f3cd18f4bdf99cb8d97f60
jjlinscientist <jjlinscientist@protonmail.com>
2018-12-14 01:28:58 -0800

    wrote tab_zero_NA() function

---
+++ R/tab_zero_NA.src.R
@@ -0,0 +1,22 @@
+#' data.table replace NAs
+#'
+#' replaces all NAs in a data.table with a set value
+#' @param data either data.table or data.frame object
+#' @param replacement value to replace NAs with
+#' @param return_data.table logical argument for table type (defaults to TRUE)
+#' @export
+#' @examples
+#' tab_zero_NA(table, replacement = 0)
+
+tab_zero_NA <- function(data,
+			replacement = 0,
+			return_data.frame = FALSE)
+{
+	data <- as.data.frame(table)
+	data[is.na(data)] <- replacement
+	if(return_data.table)
+	{
+		data <- as.data.table(data)
+	}
+	return(data)
+}
